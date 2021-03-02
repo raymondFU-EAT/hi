cafd2a30929b4ad25be035860e0ec81896c40a0b
Raymond BBF <ketchup942@gmail.com>
2021-03-03 02:27:38 +0800

    first commit

---
+++ play.py
@@ -0,0 +1,22 @@
+x=6
+print(x)
+x=5
+y=4
+print(y)
+y=7
+z=x*y
+print(z)
+price=66
+pen=9.6
+rain=True
+name='qq'
+price=65
+int=str
+str=False
+print(str, pen, rain, name)
+print(price*5, pen*5)
+H=input('請輸入身高')
+W=input('請誠實輸入體重')
+Z=input('想賺多少錢')
+print('身高', H, 'cm','也太矮了巴',
+'體重', W, '公噸','該減肥瞜', '未來財富', Z*999)
