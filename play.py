ccb7771a4918b94af1abb1454bef81e45970ddd5
raymondFU-EAT <79923758+raymondFU-EAT@users.noreply.github.com>
2021-03-03 10:37:52 +0800

    Add files via upload

--- play.py
+++ play.py
@@ -11,12 +11,11 @@ pen=9.6
 rain=True
 name='qq'
 price=65
-int=str
-str=False
-print(str, pen, rain, name)
-print(price*5, pen*5)
+print(price, pen, rain, name)
+print(price*5, pen*5 ,rain*5, name*5)
+print(rain+5)
 H=input('請輸入身高')
 W=input('請誠實輸入體重')
 Z=input('想賺多少錢')
 print('身高', H, 'cm','也太矮了巴',
-'體重', W, '公噸','該減肥瞜', '未來財富', Z*999)
+'體重', W, '公噸','該減肥瞜', '未來財富', Z*100)
